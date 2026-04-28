6b09f333a8123692ccbf16b22fec1c158ef5c368
123diso <127856684+123diso@users.noreply.github.com>
2026-04-27 19:15:09 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
+import { Route, Routes} from 'react-router-dom'
+import { IncidentsProvider } from './context/DataContext'
+import { Dashboard } from './pages/Dashboard'
+
 
 function App() {
 
 
   return (
-    <>
+
+    <IncidentsProvider>
+      <Routes>
     <p>hola</p>
-    </>
+
+    <Route path='/' element = {<Dashboard />} />
+
+      
+    </Routes>
+    </IncidentsProvider>
+
   )
 }
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,31 @@
 import { useContext, useState } from "react";
 import { DataContext } from "../context/DataContext";
 import { InitialCard } from "../Component/Card";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
 
     const {dataList}= useContext(DataContext)
 
+    const navigate = useNavigate ();
+
     const [filteredList,setFilteredList] = useState (dataList);
 
-    return (<>
-    <div>
-        {filteredList.map((
-    </div>
+    return <>
+    
+    <button onClick={() => navigate("/") }>
+
+
+    </button>
+
+      { filteredList.map(( card) =>
+        <InitialCard key={card.id} card = {card}
+        
+        />
+      ) }
+      
+      
     
     
-    </>)
+    </>
 }
